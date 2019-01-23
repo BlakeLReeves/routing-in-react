f65546ef8c682f2d29eef0d0a1804a62af9039ad
blakereeves19 <blake.reeves19@gmail.com>
2019-01-23 11:11:06 -0600

    Created new repo

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+//import { BrowserRouter as Router, Link, Switch, Route } from 'react-router';
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Hello React</h1>
+        );
+    }
+}
+
+export default App;
